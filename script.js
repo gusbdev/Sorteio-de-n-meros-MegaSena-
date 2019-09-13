5f2546279e41d4bd4a5547113288df954010a90e
Gustavo Barbosa <gusbdev@gmail.com>
2019-09-12 21:40:09 -0300

    Feito com JS

---
+++ script.js
@@ -0,0 +1,30 @@
+function sortear(){
+var sorteio;
+var mega = new Array();
+
+var i=0;
+while (i < 6){
+
+    sorteio = Math.ceil(Math.random()*60);
+    if( mega.indexOf(sorteio)< 0){
+        mega.push(sorteio);
+        i++;
+    }
+
+    function orderNumbers(a,b){
+        return a-b;     
+    }
+}
+mega.sort(orderNumbers);
+document.getElementById('num1').value = mega[0];
+document.getElementById('num2').value = mega[1];
+document.getElementById('num3').value = mega[2];
+document.getElementById('num4').value = mega[3];
+document.getElementById('num5').value = mega[4];
+document.getElementById('num6').value = mega[5];
+}
+
+
+
+
+
